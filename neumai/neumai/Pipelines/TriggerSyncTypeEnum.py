868b64d05d51d69ddc39ec7c1ed264cdab92db87
David de Matheu <ddematheu@gmail.com>
2023-11-07 16:36:24 -0800

    misc changes

--- neumai/neumai/Pipelines/TriggerSyncTypeEnum.py
+++ neumai/neumai/Pipelines/TriggerSyncTypeEnum.py
@@ -1,11 +1,8 @@
 from enum import Enum
 
 class TriggerSyncTypeEnum(str, Enum):
-    auto = "auto"       # This is eventually to support real time, right now we are just supporting and changing to delta behind the scenes
     delta = "delta"
     full = "full"
-    delta_from_time = "delta_from_time"
-    
 
     def as_trigger_sync_type(trigger_sync_type: str):
         if trigger_sync_type == None or trigger_sync_type == "":
