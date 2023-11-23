1bb9c344a15a79592fd8145fe9cf5b7ccf783e79
David de Matheu <ddematheu@gmail.com>
2023-11-23 11:54:41 -0800

    remove pipeline_id and task_id

--- neumai/neumai/Pipelines/Pipeline.py
+++ neumai/neumai/Pipelines/Pipeline.py
@@ -145,14 +145,14 @@ class Pipeline(BaseModel):
                             for chunks in source.chunk_data(document=document):
                                 embeddings, embeddings_info = self.embed.embed(documents=chunks)
                                 vectors_to_store = [NeumVector(id=str(uuid4()), vector=embeddings[i], metadata=chunks[i].metadata) for i in range(0,len(embeddings))]
-                                total_vectors_stored += self.sink.store(vectors_to_store=vectors_to_store, pipeline_id=self.id)
+                                total_vectors_stored += self.sink.store(vectors_to_store=vectors_to_store)
             return total_vectors_stored
         except Exception as e:
             raise e
     
     def search(self, query:str, number_of_results:int) -> List[NeumSearchResult]:
         vector_for_query = self.embed.embed_query(query=query)
-        matches =  self.sink.search(vector=vector_for_query, number_of_results=number_of_results, pipeline_id=self.id)
+        matches =  self.sink.search(vector=vector_for_query, number_of_results=number_of_results)
         return matches
 
     # Todo standardize the model serialization as we are mixing FE and BE concepts into the SDK
