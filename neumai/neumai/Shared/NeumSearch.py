4a1514be87081f0fd1a6f88f6a62a3434c328368
David de Matheu <ddematheu@gmail.com>
2023-12-08 14:59:31 -0800

    undo vector change in NeumSearchResult

--- neumai/neumai/Shared/NeumSearch.py
+++ neumai/neumai/Shared/NeumSearch.py
@@ -7,4 +7,3 @@ class NeumSearchResult(BaseModel):
     id:str = Field(..., description="Search result vector ID")
     metadata:dict = Field(...,description="Search result vector metadata")
     score:float =  Field(..., description="Search result similarity score")
-    vector: Optional[List[float]] = Field(..., description="Search result vector")
